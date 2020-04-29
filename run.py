650652477ffd1a1f79cb8e93b53847568a2c56db
Anja From <anjarfrom@gmail.com>
2020-04-29 20:38:38 +0200

    hgkjk

--- run.py
+++ run.py
@@ -3,15 +3,12 @@ import config
 
 
 def run_multiple():
-    config.episodes = 500  # 500
-    lr = 0.0001  # [0.0001, 0.0005, 0.001, 0.005]
+    config.lr = 0.0001  # [0.0001, 0.0005, 0.001, 0.005]
     epochs = [10, 50]
-    for l in lr:
-        config.lr = l
-        for e in epochs:
-            config.epochs = e
-            player = StateManager()
-            player.play_game()
+    for e in epochs:
+        config.epochs = e
+        player = StateManager()
+        player.play_game()
 
 
 if __name__ == "__main__":
