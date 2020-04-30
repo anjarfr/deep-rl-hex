9a87f3c455d0551ec19028274c442aa5ea9019f6
Anja <anjarfrom@gmail.com>
2020-04-30 07:40:53 +0200

    run models and edit topp config

--- config.py
+++ config.py
@@ -16,18 +16,18 @@ plot_window_size = 20
 
 # |------------- Agent parameters -------------|
 
-episodes = 500  # number of episodes
+episodes = 200  # number of episodes
 simulations = (
     500  # number of simulations (and hence rollouts) per actual game move
 )
 c = 1  # exploration constant
 m = 10  # interval of ANETs to be cached for playing TOPP
 g = 1  # number of games played in TOPP
-mcts_epsilon = 0.05
+mcts_epsilon = 0
 
 learning_rate = 0.0005
 epsilon = 1
-epsilon_decay = 0.993
+epsilon_decay = 0.98
 dimensions = [64, 64, 64]
 # Number of nodes in each layer goood = en del layers med f�rre eller 1 med flere 1000
 activation_hidden = (
@@ -35,8 +35,8 @@ activation_hidden = (
 )
 optimizer = "adam"  # Choose between 'adagrad', 'sgd', 'rmsprop' and 'adam'
 epochs = 50
-batch_size = 128
-max_buffer_length = 2500
+batch_size = 32
+max_buffer_length = 1000
 save_directory = "ep{}_sim{}_epo{}_dim{}_lr{}_bs{}_max{}".format(
     episodes,
     simulations,
